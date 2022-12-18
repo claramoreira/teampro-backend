fc7b11df89343d063901809625107e3b40d8e232
Clara <clara_ljm@hotmail.com>
2022-12-18 12:17:21 -0300

    first commit

---
+++ src/main/java/com/claramoreira/teampro/TeamproBackendApplication.java
@@ -0,0 +1,13 @@
+package com.claramoreira.teampro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamproBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamproBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claramoreira/teampro/TeamproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claramoreira.teampro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
