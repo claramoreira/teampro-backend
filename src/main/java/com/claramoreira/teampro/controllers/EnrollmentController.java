879128b623e457a9a227eb952261059d087df7b0
Clara <clara_ljm@hotmail.com>
2023-01-03 21:44:28 -0300

    enrollment getById

--- src/main/java/com/claramoreira/teampro/controllers/EnrollmentController.java
+++ src/main/java/com/claramoreira/teampro/controllers/EnrollmentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.claramoreira.teampro.domain.Enrollment;
+import com.claramoreira.teampro.domain.Team;
 import com.claramoreira.teampro.domain.User;
 import com.claramoreira.teampro.services.EnrollmentService;
 import com.claramoreira.teampro.services.UserService;
@@ -27,6 +28,12 @@ public class EnrollmentController {
 	public ResponseEntity<List<Enrollment>> findAll() {
 		List<Enrollment> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Enrollment> findById(@PathVariable Integer id) throws AttributeNotFoundException {
+		Enrollment obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
